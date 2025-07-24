64c8fd662d5e7e987679a67c55d31105404472ba
Gabriel Káicon <gkaicon2@gmail.com>
2025-07-24 14:19:48 -0300

    fix: correções na home e no gitignore

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <div className='flex items-center justify-center'>
-      <div className="container text-center px-8 py-14 rounded-2xl m-28 w-7/12" style={{backgroundColor: 'rgba(255, 255, 255, 0.60)'}}>
+      <div className="container text-center px-8 py-14 rounded-2xl m-28 w-9/12" style={{backgroundColor: 'rgba(255, 255, 255, 0.60)'}}>
         <h1 className="text-5xl font-extrabold text-black mb-4">
           Bem-vindo ao Sistema de Gestão
         </h1>
@@ -11,20 +11,20 @@ export default function HomePage() {
           Gerencie seus produtos e vendas de forma simples e eficiente.
         </p>
         <div className="flex justify-center gap-8">
-          <Link href="/products" className="group block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 transition-all">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">
-              Gerenciar Produtos
-            </h5>
-            <p className="font-normal text-black">
-              Adicione, visualize, edite e remova produtos do seu inventário.
-            </p>
-          </Link>
           <Link href="/sells/new" className="group block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 transition-all">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">
               Realizar Nova Venda
             </h5>
             <p className="font-normal text-black">
               Crie um novo registro de venda e atualize seu estoque automaticamente.
+            </p>
+          </Link>
+          <Link href="/products" className="group block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 transition-all">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">
+              Gerenciar Produtos
+            </h5>
+            <p className="font-normal text-black">
+              Adicione, visualize, edite e remova produtos do seu inventário.
             </p>
           </Link>
           <Link href="/buys/new" className="group block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 transition-all">
