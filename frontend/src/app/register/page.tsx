2b6f07f0ce390ae840733fa95fe64898199c3dc4
Gabriel Káicon <gkaicon2@gmail.com>
2025-07-31 01:43:35 -0300

    feature: register

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,108 +1,94 @@
- 
- 'use client';
-
+'use client';
 import { useState, FormEvent } from 'react';
 import api from '@/services/api';
 
 
 const RegisterPage = () => {
-  
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
- const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
 
-    try {
-      const response = await api.post('/register', { email, password });
-      const { access_token } = response.data;
-    } catch (err: any) {
-      console.error(err);
-    }
-  };
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-  return (
-    <div className="min-h-screen flex items-center justify-center ">
-      <div className="w-full max-w-md p-8 space-y-6 rounded-lg shadow-md bg-white text-black">
-        <h1 className="text-2xl font-bold text-center">
-          Registrar sua Conta
-        </h1>
 
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-black "
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="bg-gray-50 border text-black sm:text-sm rounded-lg outline-none block w-full p-2.5 "
-              placeholder="nome@companhia.com"
-              required
-            />
-          </div>
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
 
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-black "
-            >
-              Senha
-            </label>
-            <div className="relative">
-              <input
-                type={showPassword ? 'text' : 'password'}
-                name="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="••••••••"
-                className="bg-gray-50 border sm:text-sm rounded-lg outline-none block w-full p-2.5 "
-                required
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
-              >
-                {showPassword ? <EyeSlah /> : <Eye />}
-              </button>
+        try {
+            const response = await api.post('/register', { email, password });
+        } catch (err: any) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center ">
+            <div className="w-full max-w-md p-8 space-y-6 rounded-lg shadow-md bg-white text-black">
+                <h1 className="text-2xl font-bold text-center">
+                    Registrar sua Conta
+                </h1>
+
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-black "
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="bg-gray-50 border text-black sm:text-sm rounded-lg outline-none block w-full p-2.5 "
+                            placeholder="nome@companhia.com"
+                            required
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-black "
+                        >
+                            Senha
+                        </label>
+                        <div className="relative">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="••••••••"
+                                className="bg-gray-50 border sm:text-sm rounded-lg outline-none block w-full p-2.5 "
+                                required
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPassword(!showPassword)}
+                                className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
+                            >
+                                {showPassword ? <EyeSlah /> : <Eye />}
+                            </button>
+                        </div>
+                    </div>
+
+                </form>
             </div>
-          </div>
-
-          {error && (
-            <p className="text-sm font-medium text-red-500 text-center">
-              {error}
-            </p>
-          )}
-
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-blue-400 disabled:cursor-not-allowed dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            {isLoading ? 'Entrando...' : 'Entrar'}
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export const Eye = (props: React.SVGProps<SVGSVGElement>) => <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
 </svg>
 
 export const EyeSlah = (props: React.SVGProps<SVGSVGElement>) => <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
 </svg>
 
 
-export default LoginPage;
+export default RegisterPage;
